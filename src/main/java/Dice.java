c0867cb83a52d007b4282b60d148bd6fa6c91a23
Calvin <calvin@Zipcoders-MacBook-Pro.local>
2022-11-10 13:55:05 -0500

    completed except for sim obj and stars print

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,18 @@
+import java.util.Random;
+import java.lang.Math;
+
 public class Dice {
+    public int numOfDice;
+    public Dice(int numOfDice) { //Create dice
+        this.numOfDice = numOfDice;
+    }
 
-
+    public int tossAndSum() { //rolls dice
+        Random rand = new Random();
+        int sum = 0;
+        for(int i = 0 ; i < numOfDice ; i++) { //rolls amount of die
+           sum += rand.nextInt(6) + 1; //adds result to get sum
+        }
+        return sum;
+    }
 }
--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,5 +1,31 @@
+import java.util.Random;
+
 public class Simulation {
 
+    public static void main(String[] args) {
 
+        int numOfDice = 3;
+        int amountOfRolls = 10000;
+        int binUpperLimit = numOfDice * 6;
+
+        Dice dice = new Dice(numOfDice); //creates plural obj & set num of die
+        Bins batchOfBins = new Bins(numOfDice, binUpperLimit); //creates a map of bins, ex. 2 to 12
+
+        runSim(dice, batchOfBins, amountOfRolls);
+        displayResult(batchOfBins, numOfDice, binUpperLimit);
+    }
+
+    public static void runSim(Dice dice, Bins batchOfBins, int amountOfRolls) {
+        for(int i = 0 ; i < amountOfRolls ; i++) {
+            batchOfBins.addRollValuetoBin(dice.tossAndSum());
+        }
+    }
+    public static void displayResult(Bins batchOfBins, int numOfDice, int binUpperLimit) {
+        System.out.println("");
+        for(int i = numOfDice; i <= binUpperLimit ; i++) {
+            System.out.println(String.format("%2s | %10s | %.2f ", i,
+                    batchOfBins.getSpecificBin(i).toString(), batchOfBins.rollPercPerBin(i)));
+        }
+    }
 
 }
